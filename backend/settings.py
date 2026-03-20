d30f12dd7f04a168cd80a2847ccdc789a58064ff
TadaisheChibondo <n02428418x@students.nust.ac.zw>
2026-03-20 20:48:02 +0200

    Clean settings: Remove duplicate middleware and deprecated staticfiles storage

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Put this at the very top
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # 5. CORS (Allow Vercel)
 CORS_ALLOWED_ORIGINS = [
@@ -146,7 +144,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-         'rest_framework.authentication.TokenAuthentication', 
+        'rest_framework.authentication.TokenAuthentication', 
     ],
 }
 
